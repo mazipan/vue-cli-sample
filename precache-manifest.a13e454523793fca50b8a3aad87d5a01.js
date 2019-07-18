6ac2c6ee0778bc0b2e1e88835f211843e8618529
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-18 04:52:27 +0000

    Deploy mazipan/vue-cli-sample to github.com/mazipan/vue-cli-sample.git:gh-pages

---
+++ precache-manifest.a13e454523793fca50b8a3aad87d5a01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ed4a824f778d8578fb6b",
+    "url": "/js/chunk-vendors.519c4ef7.js"
+  },
+  {
+    "revision": "ce5d818cf04bb4626e36",
+    "url": "/js/app.d7020203.js"
+  },
+  {
+    "revision": "67f779ae2c3d2e08a0249346ac05888f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ce5d818cf04bb4626e36",
+    "url": "/css/app.12a77d78.css"
+  },
+  {
+    "revision": "6a02faf7ea2a9584134ffe15779a0e44",
+    "url": "/_redirects"
+  }
+];
--- src/views/About.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="about">
-    <h1>
-      This project is for learning purpose
-    </h1>
-  </div>
-</template>
